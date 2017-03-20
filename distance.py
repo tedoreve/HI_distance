6131ec37af837296728b9256c3f8a642159fbbd7
tedoreve <wlzzs666@gmail.com>
2017-03-20 18:27:56 +0800

    auto commit

--- distance.py
+++ distance.py
@@ -28,38 +28,38 @@ def box(data,head,contrast,name,region,onoff,*args):
     if data.ndim == 2:
         result0 = data[y1:y2,x1:x2]
     if data.ndim == 3:
-        result0 = data[args[0],y1:y2,x1:x2] 
+        result0 = data[:,y1:y2,x1:x2] 
     result0 = (result0-np.mean(result0))+np.mean(result0)*contrast
     
-    plt.subplots() 
-    plt.title(name)
-    plt.imshow(result0,origin='lower',interpolation='nearest',extent=[l2,l1,b1,b2])
-    plt.colorbar()
-    plt.xlabel(r'$l (deg)$')
-    plt.ylabel(r'$b (deg)$')    
-    
-    Path = mpath.Path
-    path_data = [
-        (Path.MOVETO, (onoff[0], onoff[2])),
-        (Path.CURVE4, (onoff[0], onoff[3])),
-        (Path.CURVE4, (onoff[1], onoff[3])),
-        (Path.CURVE4, (onoff[1], onoff[2])),
-        (Path.CLOSEPOLY, (onoff[0], onoff[2])),   
-        ]
-    codes, verts = zip(*path_data)
-    path = Path(verts, codes)
-    x, y = zip(*path.vertices)
-    plt.plot(x, y, 'go-')
-    plt.xlim(l2,l1)
-    plt.ylim(b1,b2)
-    plt.grid()
+#    plt.subplots() 
+#    plt.title(name)
+#    plt.imshow(result0,origin='lower',interpolation='nearest',extent=[l2,l1,b1,b2])
+#    plt.colorbar()
+#    plt.xlabel(r'$l (deg)$')
+#    plt.ylabel(r'$b (deg)$')    
+#    
+#    Path = mpath.Path
+#    path_data = [
+#        (Path.MOVETO, (onoff[0], onoff[2])),
+#        (Path.CURVE4, (onoff[0], onoff[3])),
+#        (Path.CURVE4, (onoff[1], onoff[3])),
+#        (Path.CURVE4, (onoff[1], onoff[2])),
+#        (Path.CLOSEPOLY, (onoff[0], onoff[2])),   
+#        ]
+#    codes, verts = zip(*path_data)
+#    path = Path(verts, codes)
+#    x, y = zip(*path.vertices)
+#    plt.plot(x, y, 'go-')
+#    plt.xlim(l2,l1)
+#    plt.ylim(b1,b2)
+#    plt.grid()
     
     x1,y1,x2,y2 = z.coo_box(head,onoff)  
     if data.ndim == 2:
         result = data[y1:y2,x1:x2]
     if data.ndim == 3:
         result = data[:,y1:y2,x1:x2]
-    return result
+    return result,result0
 
 
 def circle(data,head,contrast,name,region,onoff,*args):
@@ -104,6 +104,25 @@ def circle(data,head,contrast,name,region,onoff,*args):
                 if (i-result.shape[1]/2)**2+(j-result.shape[2]/2)**2 > r**2:
                     result[:,i,j]=0
     return result,result0
+
+def con(head,cont_on,region,levels):
+    '''
+    plot box pixel coordinates
+    '''
+    l1,l2,b1,b2 = region  
+    x1,y1,x2,y2 = z.coo_box(head,region)
+    
+    plt.subplots() 
+    plt.contour(cont_on,levels=levels,origin='lower',interpolation='nearest',extent=[l2,l1,b1,b2])
+    plt.colorbar()
+    plt.xlabel(r'$l (deg)$')
+    plt.ylabel(r'$b (deg)$')    
+    
+    plt.xlim(l2,l1)
+    plt.ylim(b1,b2)
+    plt.grid()
+    
+    
     
 #------------------about spectra,rotation curve,Jy/beam->K---------------------    
 def velocity(data,head):
@@ -147,9 +166,9 @@ def continuum(file,analyze,region,on,off,contrast):
     
     if analyze == 'box':
         if on != []:
-            cont_on  = box(cont_data,cont_head,1,'cont_on',region,on)
+            cont_on,cont_reg  = box(cont_data,cont_head,1,'cont_on',region,on)
         if off != []:
-            cont_off = box(cont_data,cont_head,1,'cont_off',region,off)
+            cont_off,cont_reg = box(cont_data,cont_head,1,'cont_off',region,off)
     elif analyze == 'circle':
             cont_on,cont_reg  = circle(cont_data,cont_head,1,'1420MHz continuum (K)',region,on)
             cont_off,cont_reg = circle(cont_data,cont_head,1,'cont_off',region,off)
@@ -172,9 +191,9 @@ def spectra(file,analyze,region,on,off,contrast,spec_v):
         
     if analyze == 'box':
         if on != []:
-            spec_on  = box(spec_data,spec_head,1,'spec_on',region,on,spec_v)
+            spec_on,spec_reg  = box(spec_data,spec_head,1,'spec_on',region,on,spec_v)
         if off != []:    
-            spec_off = box(spec_data,spec_head,1,'spec_off',region,off,spec_v)
+            spec_off,spec_reg = box(spec_data,spec_head,1,'spec_off',region,off,spec_v)
     elif analyze == 'circle':
         if on != []:
             spec_on,spec_reg  = circle(spec_data,spec_head,1,'1720MHz spectrum map (K) at '+str(int(v[spec_v]))+' m/s',region,on,spec_v)
@@ -183,7 +202,7 @@ def spectra(file,analyze,region,on,off,contrast,spec_v):
     
     return spec_on,spec_off,v
 #=============================absorption=======================================
-def absorption_spec(spec_on,spec_off,v,spec_on_co,spec_off_co,v_co,spec_on_vgps,spec_off_vgps,v_vgps,cont_on,cont_off,cont_on_vgps,cont_off_vgps,on,off,analyze,method): 
+def absorption_spec(spec_on,spec_off,v,spec_on_co,spec_off_co,v_co,v0,d0,cont_on,cont_off,on,off,analyze,method): 
     if analyze   == 'box':
         if method == 'tww':
             T_on     = np.mean(np.mean(spec_on,axis=1),axis=1)
@@ -196,7 +215,7 @@ def absorption_spec(spec_on,spec_off,v,spec_on_co,spec_off_co,v_co,spec_on_vgps,
                         /(cont_off.shape[0]*cont_off.shape[1]-cont_on.shape[0]*cont_on.shape[1])
                         
             T_on_co  = np.mean(np.mean(spec_on_co,axis=1),axis=1)
-            T_on_vgps= np.mean(np.mean(spec_on_vgps-cont_on_vgps,axis=1),axis=1)
+#            T_on_vgps= np.mean(np.mean(spec_on_vgps-cont_on_vgps,axis=1),axis=1)
 #            T_off_co = (np.sum(np.sum(spec_off_co,axis=1),axis=1)                    \
 #                        -(np.sum(np.sum(spec_on_co,axis=1),axis=1)))                 \
 #                        /(spec_off_co.shape[1]*spec_off_co.shape[2]-spec_on_co.shape[1]*spec_on_co.shape[2])
@@ -223,37 +242,44 @@ def absorption_spec(spec_on,spec_off,v,spec_on_co,spec_off_co,v_co,spec_on_vgps,
  
     v     = v/1000
     v_co  = v_co/1000
-    v_vgps= v_vgps/1000
-    fig, (ax1, ax2) = plt.subplots(2, sharex=True)
-    x1 = ax1.plot(v[75:200], T_on[75:200])
-    x2 = ax1.plot(v[75:200], T_off[75:200])
-    ax4= ax1.twinx()
-    x4 = ax4.plot(v_vgps,T_on_vgps,color='r')
     
-    xx = x1 + x2 +x4
-    labs = ['HI_on','HI_off','HI_VGPS']
-
+    fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
+    x1 = ax1.plot(v , T_on )
+    x2 = ax1.plot(v , T_off )
+    ax1.plot([138]*len(list(range(-80,20))),list(range(-80,20)),'--',color='purple')
+#    ax4= ax1.twinx()
+#    x4 = ax4.plot(v_vgps,T_on_vgps,color='r')
+   
+    xx = x1 + x2
+    labs = ['HI_on','HI_off']
     props = font_manager.FontProperties(size=10)
     ax1.legend(xx, labs, loc='lower right', shadow=True, prop=props)
     
     ax1.set_ylabel('T(K)')
-    ax4.set_ylabel('T(K)')
+#    ax4.set_ylabel('T(K)')
     ax1.set_title('Spectrum of G15.9+0.2')
 #    ax1.set_ylim(y2lim[0],y2lim[1])
 
-    lns2 = ax2.plot(v[75:200], e_tau[75:200])
-    ax2.plot(v[75:200],[1]*len(v[75:200]),'--')
-    ax3  = ax2.twinx()
-    lns3 = ax3.plot(v_co, T_on_co,color='r')
+    x1 = ax2.plot(v , e_tau )
+    ax2.plot(v ,[1]*len(v ),'--',color='purple')
+    ax2.plot([138]*len(np.arange(-0.25,1.5,0.05)),np.arange(-0.25,1.5,0.05),'--',color='purple')
+    ax22  = ax2.twinx()
+    x2 = ax22.plot(v_co, T_on_co,color='r')
     
-    lns = lns2 + lns3
+    xx = x1 + x2
     labs = ['HI','CO']
-    ax2.legend(lns, labs, loc='lower right', shadow=True, prop=props)
+    ax2.legend(xx, labs, loc='lower right', shadow=True, prop=props)
     
-    ax2.set_xlim(v[75],v[199])
-    ax2.set_ylabel(r'$e^{-\tau}$',fontsize=15)
-    ax2.set_xlabel('velocity(km/s)')
-    ax3.set_ylabel('T(K)')
+    ax2.set_ylabel(r'$e^{-\tau}$',fontsize=15) 
+    ax22.set_ylabel('T(K)')
+    
+    ax3.plot(v0,d0)
+    ax3.plot(v[0:177] ,[7.5]*len(v[0:177]),'--',color='purple')
+    ax3.plot([138]*len(list(range(0,20))),list(range(0,20)),'--',color='purple')
+    ax3.set_xlabel('velocity(km/s)')
+    ax3.set_ylabel('distance(kpc)')
+    ax3.set_xlim(v[75],v[199])
+    ax3.set_ylim(0,20)
 #    ax3.tick_params('y')
     
 #    ax2.plot(v_co[70:], T_off_co[70:],label='off')
@@ -261,7 +287,8 @@ def absorption_spec(spec_on,spec_off,v,spec_on_co,spec_off_co,v_co,spec_on_vgps,
 #    ax2.set_ylim(ylim[0],ylim[1])
     fig.subplots_adjust(hspace=0.05)
 #    plt.legend()
-    plt.show()   
+    plt.show()
+    return v,e_tau
 
 #=============================distance=========================================    
 def dist(model,file,l,b,d,V = 220,v_sun = 220,r_sun = 8.5):
@@ -288,26 +315,29 @@ if __name__=='__main__':
     file4   = '../data/MOS_017.Tb.fits'
     file5   = '../data/VGPS_cont_MOS017.fits'
     file6   = '../data/rotation_model.txt'
-    region  = [15.8,15.95,0.1,0.25]      #region l1,l2,b1,b2
+    region  = [15.7,16.0,0.0,0.3]      #region l1,l2,b1,b2
     on      = [15.9,15.92,0.16,0.19] 
     off     = [15.9,15.94,0.16,0.19]
     on_co   = [15.88,15.94,0.14,0.2]
     contrast = 1
     analyze  = 'box'               # box,circle
-    spec_v   = 85
+    spec_v   = 87
     model   = 'constant'            #constant, model
     V       = 220                   #km/s
     d       = np.linspace(1,40,100)
     l       = 15.9
     b       = 0.2
     method  = 'tww' #获得吸收谱的方法，tww或者classic
+    levels=[5,10,20,60,100,140]
     cont_on,cont_off    = continuum(file1,analyze,region,on,off,contrast)
     spec_on,spec_off,v  = spectra(file2,analyze,region,on,off,contrast,spec_v)
-    spec_on_co,spec_off_co,v_co  = spectra(file3,analyze,region,on_co,off,contrast,spec_v)
-    spec_on_vgps,spec_off_vgps,v_vgps  = spectra(file4,analyze,region,on,off,contrast,spec_v)
-    cont_on_vgps,cont_off_vgps    = continuum(file5,analyze,region,on,off,contrast)
-    absorption_spec(spec_on,spec_off,v,spec_on_co,spec_off_co,v_co,spec_on_vgps,spec_off_vgps,v_vgps,cont_on,cont_off,cont_on_vgps,cont_off_vgps,on,off,analyze,method)
+    spec_on_co,spec_off_co,v_co  = spectra(file3,analyze,region,on_co,off,contrast,122)
+    #v_co[120:125] T=np.sum(spec_on_co[120:125],axis=0)
     v0,d0 = dist(model,file6,l,b,d,V = 220,v_sun = 220,r_sun = 8.5)
+#    spec_on_vgps,spec_off_vgps,v_vgps  = spectra(file4,analyze,region,on,off,contrast,spec_v)
+#    cont_on_vgps,cont_off_vgps    = continuum(file5,analyze,region,on,off,contrast)
+#    v,e_tau = absorption_spec(spec_on,spec_off,v,spec_on_co,spec_off_co,v_co,v0,d0,cont_on,cont_off,on,off,analyze,method)
+    
     
 
 
